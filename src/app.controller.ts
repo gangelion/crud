b822e351aeb7571a8d6687903882e02bece138e1
tetsuya_kojima <gangelion8@gmail.com>
2022-09-21 15:54:27 +0900

    test error

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+interface MenuItem {
+  title?: string
+  to?: string
+  click?: Function
+  icon?: string
+}
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -8,5 +14,15 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  getErrorTest() {
+    const items: MenuItem[] | null = [
+      {
+        title: '',
+        icon: 'mdi-earth',
+      },
+    ];
+    items.unshift(undefined);
   }
 }
